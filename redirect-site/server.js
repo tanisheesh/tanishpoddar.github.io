620650d5c1a0c454cfcfdd486808f5731a529769
Tanish Poddar <tp6382@srmist.edu.in>
2026-02-19 02:17:51 +0530

    Changed Port

--- redirect-site/server.js
+++ redirect-site/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
   // Redirect all requests to new domain
